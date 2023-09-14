b0c60f82561b3bbbebf50879980c6f03575765a0
henry chun <henry.chun@cseducation.com.au>
2023-09-14 19:12:07 +1000

    building navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+export default function Logo() {
+  const router = useRouter();
+  return (
+    <Image
+      src='/images/logo.png'
+      alt='logo'
+      className='hidden md:block cursor-pointer'
+      height={100}
+      width={100}
+    />
+  );
+}
