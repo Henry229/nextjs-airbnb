f08ae3e94f18f0d7900aa04957bbbd61f5b36dce
henry chun <henry.chun@cseducation.com.au>
2023-09-21 22:25:32 +1000

    done properties

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -96,7 +96,10 @@ export default function UserMenu({ currentUser }: UserMenuProps) {
                   onClick={() => router.push('/reservations')}
                   label='My reservations'
                 />
-                <MenuItem onClick={() => {}} label='My properties' />
+                <MenuItem
+                  onClick={() => router.push('/properties')}
+                  label='My properties'
+                />
                 <MenuItem onClick={rentModal.onOpen} label='Airbnb your home' />
                 <hr />
                 <MenuItem onClick={() => signOut()} label='Logout' />
